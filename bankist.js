a39a6a051f1d601dd874b364b628da9e4fd488c7
FOJIUMA <fojiumajsl@gmail.com>
2026-03-16 04:03:20 -0700

    day 5 of steady grinding

--- bankist.js
+++ bankist.js
@@ -1,47 +1,3 @@
-const checkDogs = function (dogsJulia, dogsKate) {
-    const dogsJuliaCorrected = dogsJulia.slice(1,3);
-   
-    // dogsJuliaCorrected.splice(0,1)
-    // dogsJuliaCorrected.splice(-2);
-    const dogs = dogsJuliaCorrected.concat(dogsKate);
-    console.log(dogs);
-
-    dogs.forEach(function(age, i){
-        if(age >= 3){
-            console.log(`dog number ${i + 1} is an adult`)
-        }else if(age < 3){
-            console.log(`dog number ${i + 1} is a pup`)
-        }
-    })
-        
-
-   checkAge(dogs)
-};
-
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const account1 = {
 //   owner: 'Jonas Schmedtmann',
@@ -109,7 +65,7 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 
 // const displayMovements = function(movements){
@@ -126,3 +82,31 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
 //  })      
 // };
 // displayMovements(account1.movements);
+
+const eurToUsd = 1.1
+const movementsUsd = movements.map(function(mov){
+  return mov * eurToUsd;
+})
+// const movementsUsd = movements.map(mov => mov * eurToUsd) 
+
+
+console.log(movementsUsd);
+console.log(movements);
+
+const movementsUSDof = []
+
+for(const mov of movements) movementsUSDof.push(mov * eurToUsd)
+console.log(movementsUSDof)
+
+const movementsDescription = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew' } ${Math.abs(mov)}`
+// {
+//     if(mov > 0){
+//         return `Movement ${i + 1}: You deposited ${mov}` 
+//     }else{
+//         return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`
+//     }
+// }
+)
+
+
+console.log(movementsDescription)
